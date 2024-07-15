ea4e3f6b0ef289916e970686ea5f891e1488a24d
Damian <damiangperez@hotmail.com>
2024-07-15 20:13:43 -0300

    localhost

--- db.py
+++ db.py
@@ -1,16 +1,9 @@
 import pymysql
 
-# local -> wamp
-# host = "localhost"
-# user = "root"
-# clave= ""
-# db="tienda_py"
-
-# remota -> pythonenaywhere
-host = "guidovarela.mysql.pythonanywhere-services.com"
-user = "guidovarela"
-clave= "codo2024"
-db="guidovarela$tienda-py"
+host = "localhost"
+user = "root"
+clave= ""
+db="tiendaanimales"
 
 def conexionMySQL():
     return pymysql.connect(host=host,user=user,password=clave,database=db)
