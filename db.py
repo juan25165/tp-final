4773455ff8dd7d5f7f9999cec958bc9a43748eab
Damian <damiangperez@hotmail.com>
2024-07-17 20:53:49 -0300

    cambio db

--- db.py
+++ db.py
@@ -1,14 +1,17 @@
 import pymysql
 
-host = "localhost"
-user = "root"
-clave= ""
-db="tiendaanimales"
+host = "unicornio82.mysql.pythonanywhere-services.com"
+user = "unicornio82"
+clave= "Lobito460912"
+db="unicornio82$Tienda-de-Mascotas"
 
 def conexionMySQL():
     return pymysql.connect(host=host,user=user,password=clave,database=db)
 
-# host = "unicornio82.mysql.pythonanywhere-services.com"
-# user = "unicornio82"
-# clave= "Lobito460912"
-# db="unicornio82$Tienda-de-Mascotas"
+# db local
+
+# host = "localhost"
+# user = "root"
+# clave= ""
+# db="tiendaanimales"
+
